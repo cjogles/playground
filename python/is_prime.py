ccd063a6e05443488d3ff9b83875414e6fa96b15
cjogles <jackson.ogles28@gmail.com>
2020-06-30 20:11:18 -0600

    sieve_of_eratosthenes

--- python/is_prime.py
+++ python/is_prime.py
@@ -4,23 +4,25 @@
 # Write a program to determine if a number, given on the command line, is prime.
 
 def solution(num):
-    if (num >= 1):
-        for x in range(1, num):
-            print(x, "---x")
-            # if (num % x == 0):
-            #     return f"{num} is NOT a prime number"
-            # else: 
-            #     return f"{num} IS a prime number."
-    elif (num == 0):
-        return f"{num} is NOT a prime number."
+    if (num < 1):
+        return f"{num} is not a prime number"
+    else:
+        for x in range(2, num):
+            if (num % x) == 0:
+                return f"{num} is not a prime number"
+    return f"{num} is a prime number"
 
 
 # TEST
-print(solution(3))  # should return string ==> "{number}is a prime number."
+print(solution(13))  # should return string ==> "{number}is a prime number."
 
 # My explanation for this problem:
 # - In order for a number to be considered "PRIME", it needs to be greater then 1, and its only factors can be 1 and itself.
-# - To solve this, I would first take care of the edge case of it being less then 1.
+# - To solve this, I would first take care of the edge case of it being less then 1, if so, its not prime.
+# - next, I realize that every non-negative integer greater then 0 has 1 and itself as its factors. I just need to check if any other
+#   number is a factor of the given number. So I take 2 through (num -1), loop through each one of them and run the following:
+#   num % x == 0... if that evaluates to true, that means there is a factor of num outside of 1 and itself, which makes it NOT prime. 
+#   otherwise return prime
 
 # TIME COMPLEXITY
 # -
